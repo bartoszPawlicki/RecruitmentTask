5cdc9ca32496562b9422b2e03ba31ac86b9cf2cf
Bartosz Pawlicki <bartosz.pawlicki@gecko-lab.com>
2025-11-03 16:06:33 +0100

    Add basic movement, enemy creation WIP

--- RecruitmentTask/Assets/Scripts/GameManager.cs
+++ RecruitmentTask/Assets/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine.InputSystem;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public GameObject enemyPrefab;
+    public int EnemiesToSpawn = 1000;
+    public float playerSpeed;
+
+    public GameObject player;
 
     public GameObject mainMenu, settings;
 
@@ -40,6 +45,27 @@ public class GameManager : MonoBehaviour
                 instance.gameState = GameState.MENU;
                 instance.mainMenu.SetActive(true);
             }
+            HandleMoevement();
         }
+    }
+
+    public void CreateEnemies()
+    {
+        for (int i = 0; i < EnemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab);
+        }
+    }
+
+    public void HandleMoevement()
+    {
+        Vector2 move = Vector2.zero;
+        if (Keyboard.current.wKey.isPressed) move.y += 1;
+        if (Keyboard.current.sKey.isPressed) move.y -= 1;
+        if (Keyboard.current.aKey.isPressed) move.x -= 1;
+        if (Keyboard.current.dKey.isPressed) move.x += 1;
+
+        move = move.normalized * playerSpeed * Time.deltaTime;
+        player.transform.position += (Vector3)move;
     }
 }
